62f3249c76b55af7154b9fe63c0b4d13b4a4bc0d
Yoshihiro Furukawa <yoshi@YoshihironoMacBook-Pro.local>
2023-11-29 19:24:05 +0900

    Add initial files

---
+++ SystemStructureChecker/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  SystemStructureChecker
+//
+//  Created by Yoshihiro Furukawa on 2023/11/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+    return EXIT_SUCCESS;
+}
