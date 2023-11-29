9f805b4705cffe2d48a2484a64831709dfea41dc
Yoshihiro Furukawa <yoshi@YoshihironoMacBook-Pro.local>
2023-11-29 20:58:58 +0900

    Systemをインスタンス化するように実装した

--- SystemStructureChecker/main.cpp
+++ SystemStructureChecker/main.cpp
@@ -5,9 +5,13 @@
 //  Created by Yoshihiro Furukawa on 2023/11/29.
 //
 
+#include <memory>
 #include <iostream>
+#include <string>
+#include "System.h"
 
 int main(int argc, const char * argv[]) {
-
+    auto system = std::make_unique<System>("System");
+    
     return EXIT_SUCCESS;
 }
