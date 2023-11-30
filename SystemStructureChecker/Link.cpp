5bd8d49370d0b9f0135c3fe9dba682cae6bf129f
Yoshihiro Furukawa <yoshi@YoshihironoMacBook-Pro.local>
2023-11-30 23:17:34 +0900

    Interfaceをバインドできるようにした

--- SystemStructureChecker/Link.cpp
+++ SystemStructureChecker/Link.cpp
@@ -8,3 +8,9 @@ std::string Link::name()
 {
     return linkName_;
 }
+
+void Link::bindInterface(std::shared_ptr<Interface> lhs, std::shared_ptr<Interface> rhs)
+{
+    lhs_ = lhs;
+    rhs_ = rhs;
+}
--- SystemStructureChecker/Link.h
+++ SystemStructureChecker/Link.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <memory>
 #include <string>
+
+class Interface;
 
 class Link
 {
@@ -10,6 +13,9 @@ public:
     explicit Link( std::string linkName );
     ~Link() = default;
     std::string name();
+    void bindInterface( std::shared_ptr<Interface> lhs, std::shared_ptr<Interface> rhs );
 private:
     std::string linkName_{};
+    std::shared_ptr<Interface> lhs_{};
+    std::shared_ptr<Interface> rhs_{};
 };
