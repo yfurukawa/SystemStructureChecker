deea987b6d6f2dbb1095e995e00163cf4c3fde46
Yoshihiro Furukawa <yoshi@YoshihironoMacBook-Pro.local>
2023-11-30 23:28:53 +0900

    <<interface>>として使えるように基本的な部分のみ実装した

--- SystemStructureChecker/FlowObject.h
+++ SystemStructureChecker/FlowObject.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#include <memory>
+#include <string>
+
 class FlowObject
 {
-    
+private:
+    FlowObject() = delete;
+public:
+    explicit FlowObject( std::string name ) : objectName_( std::make_unique<std::string>( std::move(name)) ){};
+    virtual FlowObject() = default;
+protected:
+    std::unique_ptr<std::string> objectName_{};
 };
